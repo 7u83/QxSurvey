b3171ab32ec5ed2784a09aefe3040105d9142743
7u83 <7u83@mail.ru>
2015-02-14 16:12:03 +0100

    Function to get row-width added.

--- QxImage_class.php
+++ QxImage_class.php
@@ -1,12 +1,10 @@
 <?php
 
 
-
-
 class QxImage extends Imagick
 {
 
-	function togrey($y)
+	function get_row($y)
 	{
 
 		$width = $this->getImagePage()['width'];
@@ -35,8 +33,79 @@ class QxImage extends Imagick
 
 		}
 
+		return array ('row'=>$row,'min'=>$min,'max'=>$max);	
+	}
+
+	function grey2bw($g,$thr)
+	{
+		if ($g>=$thr)
+			return 1;
+		return 0;
+	}
+
+
+	function get_w($row,$min,$max) 
+	{
+		$xdim = count($row);
+
+
+		$range = $max-$min;
+		$thr = $range/2.0+$min;
+
+
+		$b=array();
+
+		$col=$this->grey2bw($row[0],$thr);
+
+
+		$count=1;
+		for ($x = 1; $x < $xdim; $x++) {
+			$g=$row[$x]-$min;
+	
+			$bw = $this->grey2bw($g,$thr);	
+
+			if ($bw != $col )
+			{
+				$n = ($range-$g)/$range;	
+				$o = 1-$n;
+
+				if ($bw>$col) {
+					$o=1-$o;
+					$n=1-$n;
+					$z=-1;
+				}
+				else{
+					$z=1;
+				}
+
+				$count+=$o;
+				
+				$b[] = $count*$z;
+
+				$count=$n;
+				$col = $bw;
+			}
+			$count++;	
+		}
+
+
+		return $b;
+	}
+
+
+	function get_bc($y)
+	{
+		$r = $this->get_row(176);
+
+		$r = $this->get_w($r['row'],$r['min'],$r['max']);
+
+		for ( $i=0; $i<count($r); $i++){
+			printf("%0.2f|",$r[$i]);
+		}
+
 		// Based on min and max convert to BW	
-		
+	
+/*	
 		$thr = ($max-$min)/2.0+$min;
 
 		for($x=0; $x<$width; $x++){
@@ -45,14 +114,7 @@ class QxImage extends Imagick
 			else
 				$row[$x]=0;
 		}
-	
-		return $row;	
-	}
-
-	function get_bc($y)
-	{
-		$this->togrey(176);
-
+	*/
 		
 	}
 
@@ -63,7 +125,8 @@ class QxImage extends Imagick
 	function test()
 	{
 		$rc = $this->getImagePage();
-		$this->get_bc(141);
+
+			$this->get_bc(141);
 
 
 		var_dump($rc);
